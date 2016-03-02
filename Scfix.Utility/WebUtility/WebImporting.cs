7c5ae09ee722aa8dc742bb5a4de7e4b605ba1bba
Matthew Wilkinson <scfix@outlook.com>
2016-03-02 00:10:02 -0500

    Excluding Files

--- Scfix.Utility/WebUtility/WebImporting.cs
+++ Scfix.Utility/WebUtility/WebImporting.cs
@@ -14,165 +14,166 @@ using System.Windows;
 
 namespace ScFix.Utility.WebUtility
 {
-    public class WebManager
-    {
+	public class WebManager
+	{
 
-        //if tmpImage is null then you did not get anything from the web
+		//if tmpImage is null then you did not get anything from the web
 
-        static public Image DownloadImage(string URL)
-        {
-            Image tmpImage = null;
-            HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
-            webRequest.AllowWriteStreamBuffering = true;
-            //timesout in 10 seconds
-            webRequest.Timeout = 10000;
-            //gets the response
-            WebResponse webResponse = webRequest.GetResponse();
-            //gets the strea
-            Stream stream = webResponse.GetResponseStream();
-            tmpImage = Image.FromStream(stream);
+		static public Image DownloadImage(string URL)
+		{
+			Image tmpImage = null;
+			HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
+			webRequest.AllowWriteStreamBuffering = true;
+			//timesout in 10 seconds
+			webRequest.Timeout = 10000;
+			//gets the response
+			WebResponse webResponse = webRequest.GetResponse();
+			//gets the strea
+			Stream stream = webResponse.GetResponseStream();
+			tmpImage = Image.FromStream(stream);
 
-            return tmpImage;
-        }
+			return tmpImage;
+		}
 
-        static public bool DownloadImage(string URL, string FileLoaction)
-        {
-            try
-            {
-                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
-                webRequest.AllowWriteStreamBuffering = true;
-                //timesout in 10 seconds
-                webRequest.Timeout = 20000;
-                //gets the response
-                WebResponse webResponse = webRequest.GetResponse();
-                //gets the strea
-                Stream stream = webResponse.GetResponseStream();
-                FileInfo fi = new FileInfo(FileLoaction);
-                if (fi.Exists)
-                {
-                    throw new Exception("File already Exists");
-                }
-                else
-                {
-                    using (var fileStream = File.Create(FileLoaction))
-                    {
-                        stream.CopyTo(fileStream);
-                    }
-                    return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                return false;
+		static public bool DownloadImage(string URL, string FileLoaction)
+		{
+			try
+			{
+				HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
+				webRequest.AllowWriteStreamBuffering = true;
+				//timesout in 10 seconds
+				webRequest.Timeout = 20000;
+				//gets the response
+				WebResponse webResponse = webRequest.GetResponse();
+				//gets the strea
+				Stream stream = webResponse.GetResponseStream();
+				FileInfo fi = new FileInfo(FileLoaction);
+				if (fi.Exists)
+				{
+					throw new Exception("File already Exists");
+				}
+				else
+				{
+					using (var fileStream = File.Create(FileLoaction))
+					{
+						stream.CopyTo(fileStream);
+					}
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return false;
 
-            }
+			}
 
-        }
+		}
 
-        static public string GetWebPage(string URL)
-        {
-            try
-            {
-                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
-                webRequest.AllowWriteStreamBuffering = true;
-                //ten second timeout
-                webRequest.Timeout = 10000;
-                WebResponse WebResponse = webRequest.GetResponse();
-                Stream stream = WebResponse.GetResponseStream();
-                StreamReader sr = new StreamReader(stream);
-                string s = sr.ReadToEnd();
-                return s;
-            }
-            catch (Exception x)
-            {
-                throw new Exception("Web Page did not load", x);
-            }
-        }
+		static public string GetWebPage(string URL)
+		{
+			try
+			{
+				HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
+				webRequest.AllowWriteStreamBuffering = true;
+				//ten second timeout
+				webRequest.Timeout = 10000;
+				WebResponse WebResponse = webRequest.GetResponse();
+				Stream stream = WebResponse.GetResponseStream();
+				StreamReader sr = new StreamReader(stream);
+				string s = sr.ReadToEnd();
+				return s;
+			}
+			catch (Exception x)
+			{
+				throw new Exception("Web Page did not load", x);
+			}
+		}
 
-        static public void GetGeneratedHTML(string url, Action<string> Callback, string breakCondition = "")
-        {
-            Thread t = new Thread(Bg_DoWork);
-            t.SetApartmentState(ApartmentState.STA);
-            t.Start(new GeneratedHtmlArgs() { url = url, callback = Callback, breakCondition = breakCondition });
-
-
-        }
-
-        private static void Bg_DoWork(Object obj)
-        {
-            GeneratedHtmlArgs args = (GeneratedHtmlArgs)obj;
-            System.Windows.Forms.WebBrowser wb = new System.Windows.Forms.WebBrowser();
-            var na = new navigatingArgs() { AcceptedUrl = args.url };
-            wb.Navigating += na.Wb_Navigating;
-            wb.DocumentCompleted += na.Wb_DocumentCompleted;
-            wb.Navigate(args.url);
-
-            //wb.FileDownload += Wb_FileDownload;
-            while (!na.isLoaded || (wb.ReadyState != WebBrowserReadyState.Complete))
-            {
-                if (wb.DocumentText != "" && !wb.DocumentText.Contains("href=\"\""))
-                    if (args.breakCondition != "" && wb.DocumentText.Contains(args.breakCondition))
-                        break;
-                System.Windows.Forms.Application.DoEvents();
-
-            }
+		static public void GetGeneratedHTML(string url, Action<string> Callback, string breakCondition = "")
+		{
+			Thread t = new Thread(Bg_DoWork);
+			t.SetApartmentState(ApartmentState.STA);
+			t.Start(new GeneratedHtmlArgs() { url = url, callback = Callback, breakCondition = breakCondition });
 
 
-            //Added this line, because the final HTML takes a while to show up
-            args.finalHtml = wb.DocumentText;
+		}
+
+		private static void Bg_DoWork(Object obj)
+		{
+			GeneratedHtmlArgs args = (GeneratedHtmlArgs)obj;
+			System.Windows.Forms.WebBrowser wb = new System.Windows.Forms.WebBrowser();
+			var na = new navigatingArgs() { AcceptedUrl = args.url };
+			wb.Navigating += na.Wb_Navigating;
+			wb.DocumentCompleted += na.Wb_DocumentCompleted;
+			wb.Navigate(args.url);
+
+			//wb.FileDownload += Wb_FileDownload;
+			while (!na.isLoaded || (wb.ReadyState != WebBrowserReadyState.Complete))
+			{
+				if (wb.DocumentText != "" && !wb.DocumentText.Contains("href=\"\""))
+					if (args.breakCondition != "" && wb.DocumentText.Contains(args.breakCondition))
+						break;
+				System.Windows.Forms.Application.DoEvents();
+
+			}
 
 
-
-            System.Windows.Application.Current.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate () { args.callback(args.finalHtml); }));
-
-            wb.Dispose();
-
-
-        }
+			//Added this line, because the final HTML takes a while to show up
+			args.finalHtml = wb.DocumentText;
 
 
 
-        private static void Wb_FileDownload(object sender, EventArgs e)
-        {
-            Debug.WriteLine("whate aer you doing");
-        }
+			System.Windows.Application.Current.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate () { args.callback(args.finalHtml); }));
 
-        private static void Bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            if (!e.Cancelled && e.Error == null)
-            {
-
-            }
-        }
-
-        private class GeneratedHtmlArgs
-        {
-            public string url { get; set; }
-            public string breakCondition { get; set; }
-            public Action<String> callback { get; set; }
-
-            public string finalHtml { get; set; }
-        }
+			wb.Dispose();
 
 
-        private class navigatingArgs
-        {
-            public string AcceptedUrl { get; set; }
-            public bool isLoaded { get; set; }
+		}
 
-            public void Wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-            {
-                isLoaded = true;
-            }
-            public void Wb_Navigating(object sender, WebBrowserNavigatingEventArgs e)
-            {
-                //Debug.WriteLine("whate");
-                if (e.Url.ToString() != AcceptedUrl)
-                {
-                    //e.Cancel = true;
-                }
-            }
-        }
-    }
+
+
+		private static void Wb_FileDownload(object sender, EventArgs e)
+		{
+			Debug.WriteLine("whate aer you doing");
+		}
+
+		private static void Bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			if (!e.Cancelled && e.Error == null)
+			{
+
+			}
+		}
+
+		private class GeneratedHtmlArgs
+		{
+			public string url { get; set; }
+			public string breakCondition { get; set; }
+			public Action<String> callback { get; set; }
+
+			public string finalHtml { get; set; }
+		}
+
+
+		private class navigatingArgs
+		{
+			public string AcceptedUrl { get; set; }
+			public bool isLoaded { get; set; }
+
+			public void Wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+			{
+				if (!(sender as WebBrowser).DocumentTitle.Contains("5 seconds"))
+					isLoaded = true;
+			}
+			public void Wb_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+			{
+				//Debug.WriteLine("whate");
+				if (e.Url.ToString() != AcceptedUrl)
+				{
+					//e.Cancel = true;
+				}
+			}
+		}
+	}
 }
